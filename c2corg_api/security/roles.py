bbdef2a1c687dd668a1318836cd4b3cf87733529
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2015-11-25 14:50:08 +0100

    Purge expired tokens from the database
    Triggered by a user request.
    No more than once a day.

--- c2corg_api/security/roles.py
+++ c2corg_api/security/roles.py
@@ -16,6 +16,9 @@ next_expire_cleanup = datetime.datetime.utcnow()
 # The number of days after which a token is expired
 CONST_EXPIRE_AFTER_DAYS = 14
 
+# The number of days after which expired tokens are deleted
+CONST_CRON_DAYS = 1
+
 
 def groupfinder(userid, request):
     is_moderator = DBSession.query(User). \
@@ -36,6 +39,17 @@ def add_token(value, expire, userid):
     DBSession.flush()
 
 
+def clean_expired_tokens():
+    global next_expire_cleanup
+    now = datetime.datetime.utcnow()
+    if now >= next_expire_cleanup:
+        next_expire_cleanup = now + datetime.timedelta(days=CONST_CRON_DAYS)
+        condition = Token.expire > now
+        result = DBSession.execute(Token.__table__.delete().where(condition))
+        log.info('Removed %d expired tokens' % result.rowcount)
+        DBSession.flush()
+
+
 def remove_token(token):
     now = datetime.datetime.utcnow()
     condition = Token.value == token and Token.expire > now
@@ -43,6 +57,7 @@ def remove_token(token):
     if result.rowcount == 0:
         log.debug('Failed to remove token %s' % token)
     DBSession.flush()
+    clean_expired_tokens()
 
 
 def create_claims(user, exp):
@@ -57,6 +72,7 @@ def try_login(username, password, request):
     user = DBSession.query(User). \
         filter(User.username == username).first()
 
+    clean_expired_tokens()
     if username and password and user.validate_password(password, DBSession):
         policy = request.registry.queryUtility(IAuthenticationPolicy)
         now = datetime.datetime.utcnow()
